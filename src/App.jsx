99b7278c34187c8362e20f78052fc961d17b1d62
LucasFigueiredoDEV <lucassamuell.dev@gmail.com>
2026-04-08 02:13:35 -0300

    Enhance App component: add language and theme management, update Navbar and main content structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar/index.jsx'
 import Asidebar from './components/asidebar/index.jsx'
 import Aboutme from './components/content/about-me/index.jsx'
-import Capatibilities from './components/content/capatibilities/index.jsx'
+import Capabilities from './components/content/capatibilities/index.jsx'
 import Journey from './components/content/journey/index.jsx'
 import Projects from './components/content/projects/index.jsx'
+import Contact from './components/content/contact/index.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [darkMode, setDarkMode] = useState(true);
+
+  const [lang, setLang] = useState("en");
+
+  function handleChangeLang(newLang) {
+    setLang(newLang);
+    localStorage.setItem("language", newLang);
+  }
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem("language");
+    if (savedLang) setLang(savedLang);
+  }, []);
+
+  // Ao montar, carrega preferência salva
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+      setDarkMode(false);
+    }
+  }, []);
+
+  // Sempre que darkMode mudar, atualiza DOM e localStorage
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.remove("light-theme");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.add("light-theme");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  function handleChangeTheme(e) {
+    e.preventDefault();
+    setDarkMode((prev) => !prev);
+  }
 
   return (
     <div className="app-layout">
       <Asidebar />
-      <Navbar />
+      <Navbar darkMode={darkMode} handleChangeTheme={handleChangeTheme} defaultLang={lang} handleChangeLang={handleChangeLang} />
       <main className="main-content">
-        <Aboutme />
-        <Capatibilities />
-        <Journey />
-        <Projects />
+        <Aboutme lang={lang} />
+        <Capabilities />
+        <Journey lang={lang} />
+        <Projects lang={lang} />
+        <Contact lang={lang} />
       </main>
       <footer className='footer'>
         lulucasdev © 2024 Created by Lucas Figueiredo
